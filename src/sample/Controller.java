46bd7afe3cb52127fe92000c4e85481d15d03e3e
Michał Czaja <35144@studentwsei.pl>
2021-05-21 15:11:34 +0200

    Zadanie nr.4

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,6 +1,18 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
 public class Controller
 {
 
+    public Button btnMain;
+    int clickCounter = 0;
+
+    public void onBtnAction(ActionEvent actionEvent)
+    {
+        System.out.println("Kliknięcie nr: " + ++clickCounter);
+        if (clickCounter == 1)
+            btnMain.setText("I jeszcze raz!");
+    }
 }
