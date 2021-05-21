2dd4ffe98f7b3bf5a6dc3d369addef266898c2a0
Michał Czaja <35144@studentwsei.pl>
2021-05-21 14:55:12 +0200

    Zadanie nr.3

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,10 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+public class Main extends Application
+{
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception
+    {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
