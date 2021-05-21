b965972c92b8c9a1645d50c62a56ab98837ddadf
Michał Czaja <35144@studentwsei.pl>
2021-05-21 15:22:42 +0200

    Zadanie nr.5

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,17 +2,21 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class Controller
 {
 
     public Button btnMain;
+    public TextField txtLicznik;
     int clickCounter = 0;
 
     public void onBtnAction(ActionEvent actionEvent)
     {
-        System.out.println("Kliknięcie nr: " + ++clickCounter);
+        //System.out.println("Kliknięcie nr: " + ++clickCounter);
         if (clickCounter == 1)
             btnMain.setText("I jeszcze raz!");
+
+        txtLicznik.setText(Integer.toString(++clickCounter));
     }
 }
